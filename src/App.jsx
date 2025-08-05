b0c4d39ac02c25fabcf848a8dbf49e733bada32c
compoint401 <compoint401@gmail.com>
2025-08-05 21:53:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+     <div> Hello from React</div>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </>
+  )
+}
+
